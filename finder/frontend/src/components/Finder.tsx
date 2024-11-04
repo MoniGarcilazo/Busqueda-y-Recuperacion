2d8091725556ea654737f50c92b25277a24ec2c1
Rubén Alvarado <ruben_aalvarado@outlook.com>
2024-11-04 12:16:22 -0600

    feat: :sparkles: Main view

---
+++ finder/frontend/src/components/Finder.tsx
@@ -0,0 +1,45 @@
+import { ChangeEvent, useState } from 'react';
+
+import { IconField } from 'primereact/iconfield';
+import { InputIcon } from 'primereact/inputicon';
+import { InputText } from 'primereact/inputtext';
+
+import '../styles/Finder.css';
+
+function Finder() {
+    const [input, setInput] = useState<string>('');
+
+    const handleInputChange = (
+        event: ChangeEvent<HTMLInputElement>
+    ) => {
+        setInput(event.target.value);
+    }
+
+    const reset = () => {
+        console.log("Hola mundo...");
+        setInput('');
+    }
+
+    return (
+        <section className='flex gab-3 finder'>
+            <form id="finder" action="" method='GET' className='field'>
+                <IconField iconPosition='left'>
+                    <InputIcon className='pi pi-search'></InputIcon>
+                    <InputText
+                        placeholder="Search"
+                        className='field input-text'
+                        value={input}
+                        onChange={handleInputChange}
+                    />
+                </IconField>
+                {input !== '' && (
+                    <InputIcon
+                    className='pi pi-times'
+                    onClick={reset}></InputIcon>
+                )}
+            </form>
+        </section>
+    );
+}
+
+export default Finder;
