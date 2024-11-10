bdc3b93f8084a235f50ff1bc3165b6f5f3e793dd
Rubén Alvarado <ruben_aalvarado@outlook.com>
2024-11-10 00:50:49 -0600

    docs: :memo: Readme and requirements added for app installation.
    The readme and requirements files were added. API now accepts multiple hosts by the middleware added.
    
    4

--- finder/backend/main.py
+++ finder/backend/main.py
@@ -1,8 +1,28 @@
+from contextlib import asynccontextmanager
 from typing import Union
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
-app = FastAPI()
+from src.docs.openapi_tag import openapi_tags
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    print("Starting application")
+    yield
+    print("Shutting down application")
+
+app = FastAPI(
+    title="MIABEN API",
+    summary="API for MIABEN information search and retrieval application",
+    version="0.0.1",
+    contact={
+        "name": "Ruben Alvarado",
+        "email": "A18003305@alumnos.uady.mx",
+    },
+    openapi_tags=openapi_tags,
+    lifespan=lifespan,
+)
 
 @app.get("/")
 def read_root():
@@ -11,3 +31,11 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+    allow_credentials=True,
+)
