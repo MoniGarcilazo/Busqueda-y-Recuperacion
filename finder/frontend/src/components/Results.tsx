63b80c3ee0738c29682f284b9b36d730789b1bc9
Rubén Alvarado <ruben_aalvarado@outlook.com>
2024-12-05 12:51:15 -0600

    fix: :bug: Bugs con el snippet done

--- finder/frontend/src/components/Results.tsx
+++ finder/frontend/src/components/Results.tsx
@@ -1,6 +1,8 @@
 import { useLocation, useNavigate } from 'react-router-dom';
 import '../styles/Results.css';
 
+import { createSnippet } from '../services/AddSnippet';
+
 function Results() {
     const location = useLocation();
     const navigate = useNavigate();
